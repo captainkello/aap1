4000627a04afff427b4b8dd730e0617d747461b1
Cap Kello <captainkello@gmail.com>
2013-09-09 20:12:09 -0700

    Added routes for adopt, give, volunteer

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,7 @@ describe "StaticPages" do
   describe "GET /static_pages/home" do
     it "should have content home" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
+      visit 'root_path'
       expect(page).to have_content('home')
     end
   end
@@ -12,7 +12,7 @@ describe "StaticPages" do
   describe "GET /static_pages/adopt" do
     it "should have content adopt" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/adopt'
+      visit 'adopt_path'
       expect(page).to have_content('adopt')
     end
   end
@@ -20,7 +20,7 @@ describe "StaticPages" do
   describe "GET /static_pages/give" do
     it "should have content give" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/give'
+      visit 'give_path'
       expect(page).to have_content('give')
     end
   end
@@ -28,7 +28,7 @@ describe "StaticPages" do
   describe "GET /static_pages/volunteer" do
     it "should have content volunteer" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/volunteer'
+      visit 'volunteer_path'
       expect(page).to have_content('volunteer')
     end
   end
